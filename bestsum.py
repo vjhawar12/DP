7a97337e49857c1dcd4afa8a754a8782cbb0d939
“vjhawar12” <vedantj1203@gmail.com>
2023-09-13 11:01:19 -0400

    Second commit

--- bestsum.py
+++ bestsum.py
@@ -1,5 +1,30 @@
 # write a program that returns how a target sum can be generated using the least number of numbers 
 # numbers possible from an inputted array
 
+
 def bestsum(target, nums, dic={}):
-    if 
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+    if target in dic:
+        return dic[target]
+
+    shortest = None
+    for i in nums:
+        result = bestsum(target - i, nums)
+        
+        if result != None:
+            result.append(i)
+            if shortest == None or len(result) < len(shortest):
+                shortest = result
+    
+    return shortest
+
+print(bestsum(10, [2, 5], {}))
+print(bestsum(7, [2, 3], {}))
+print(bestsum(7, [5, 3, 4, 7], {}))
+print(bestsum(7, [2, 4], {}))
+print(bestsum(8, [2, 3, 5], {}))
+print(bestsum(300, [7, 14], {}))
+
